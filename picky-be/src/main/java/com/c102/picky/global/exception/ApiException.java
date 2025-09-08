16408288158bbd1fd1463a33627299903af7887c
정해빈 <habin1213@naver.com>
2025-09-08 10:59:21 +0900

    chore: 프로젝트 초기 스켈레톤 생성

---
+++ picky-be/src/main/java/com/c102/picky/global/exception/ApiException.java
@@ -0,0 +1,13 @@
+package com.c102.picky.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ApiException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public ApiException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
