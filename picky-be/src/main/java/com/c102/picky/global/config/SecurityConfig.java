096afa48e285f3ceafcb953b18a41c364049652b
정해빈 <habin1213@naver.com>
2025-09-10 12:13:12 +0900

    chore: 불필요한 import문 삭제

--- picky-be/src/main/java/com/c102/picky/global/config/SecurityConfig.java
+++ picky-be/src/main/java/com/c102/picky/global/config/SecurityConfig.java
@@ -2,17 +2,12 @@ package com.c102.picky.global.config;
 
 import com.c102.picky.global.security.jwt.JwtAuthenticationEntryPoint;
 import com.c102.picky.global.security.jwt.JwtAuthenticationFilter;
-import com.c102.picky.global.security.oauth2.OAuth2LoginFailureHandler;
-import com.c102.picky.global.security.oauth2.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
