5196ae72be9556be5dddd8073ed2a8c840bb5bec
정해빈 <habin1213@naver.com>
2025-09-25 15:10:29 +0900

    fix(userInterest): 카테고리 관련 로직 수정
    - 관심 카테고리 삭제 로직 추가
    - requestDto에 빈 리스트 허용

--- picky-be/src/main/java/com/c102/picky/domain/userinterest/dto/UserInterestAddRequestDto.java
+++ picky-be/src/main/java/com/c102/picky/domain/userinterest/dto/UserInterestAddRequestDto.java
@@ -1,12 +1,10 @@
 package com.c102.picky.domain.userinterest.dto;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class UserInterestAddRequestDto {
-    @NotEmpty(message = "categoryIds는 비어있을 수 없습니다.")
     private List<Long> categoryIds;
 }
