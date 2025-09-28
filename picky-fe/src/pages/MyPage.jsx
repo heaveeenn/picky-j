4df58aceb17d25b92cd2da2bf7bc163fff178087
윤지훈 <wlgnsghk1@naver.com>
2025-09-28 16:58:05 +0900

    style: 스타일변경

--- picky-fe/src/pages/MyPage.jsx
+++ picky-fe/src/pages/MyPage.jsx
@@ -375,7 +375,7 @@ const MyPage = ({ onClose, nickname, profileImage }) => {
 ) : (
   <>
                 <Box>
-          <Bookmark className="w-5 h-5 mr-2 text-primary" />스크랩한 뉴스
+                  <h3 className="text-lg font-semibold text-gray-800 mb-4 flex items-center"><Bookmark className="w-5 h-5 mr-2 text-primary" />스크랩한 뉴스</h3>
                   <div className="flex space-x-2 mb-4">
                     <input
                       type="text"
